d610871205800100b64335ea24b16320d5c61ca5
TNV <TNV@LAPTOP-ENHF7T2L>
2024-08-18 00:25:03 +0700

    Add project files.

---
+++ Internship/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Internship.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
